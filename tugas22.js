f2b8b8ed7df6b0dfc092bebbe88e6474943c34dd
Aria Bagas <aariabagass@gmail.com>
2019-08-23 22:21:11 +0700

    JS Task 22
    array split, array foreach

---
+++ tugas22.js
@@ -0,0 +1,9 @@
+var array = () => {
+  a = "Saya ingin belajar bersama"
+  split = a.split(" ");
+  split.forEach((i, j) => {
+    console.log("Item : " + i + " Index ke " + j)
+  })
+  return array;
+}
+array();
